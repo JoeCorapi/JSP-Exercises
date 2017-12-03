b746d7dd2ddd054febe716a0dfddc6a54bc0c061
Joe Corapi <24542019+JoeCorapi@users.noreply.github.com>
2017-12-03 14:17:12 -0500

    HTML form data with servlets

---
+++ servlet-demo/src/com/corapi/servletdemo/StudentServlet.java
@@ -0,0 +1,30 @@
+package com.corapi.servletdemo;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "StudentServlet", urlPatterns = {"/StudentServlet"})
+public class StudentServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+
+        out.println("<html><body>");
+
+        out.println("The student is confirmed: " + request.getParameter("firstName") + " " +
+            request.getParameter("lastName"));
+
+        out.println("</body></html>");
+    }
+}
